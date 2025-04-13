2ba531a6bca39eacf501f15d9fd7a7a9b37bfe99
xanaxdev <developer.xanax@gmail.com>
2025-04-13 11:17:09 +0200

    Update: Utworzenie konfiguracji aktów, dodanie nowych parametrów

--- lib/settings/actConfig.ts
+++ lib/settings/actConfig.ts
@@ -1,5 +1,18 @@
-export const actsConfig = [
-    { key: 'startgame', title: 'Akt 1: Rekrutacja', completedKey: 'completed_startgame' },
-    { key: 'akt-1', title: 'Akt 2: Lot na Księżyc', completedKey: 'completed_akt-1' },
-    { key: 'akt-2', title: 'Akt 3: Pierwszy Kontakt', completedKey: 'completed_akt-2' },
-];
+// acts.config.ts
+export const actsConfig = {
+  startgame: {
+    id: "startgame",
+    title: "PROLOG",
+    initialScene: "dzwoni_officer",
+  },
+  "akt-1": {
+    id: "akt-1",
+    title: "AKT I",
+    initialScene: "rozpoczecie_akt2",
+  },
+  "akt-2": {
+    id: "akt-2",
+    title: "AKT II",
+    initialScene: "rozpoczecie_aktu",
+  },
+};
