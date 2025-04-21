64e2805c0e7a6e177924aa2ef37c6f7443d724f0
xanaxdev <developer.xanax@gmail.com>
2025-04-21 14:28:26 +0200

    Add: Utworzenie helpera dla pobierania aktualnego aktu.

--- lib/helpers/getCurrentAct.ts
+++ lib/helpers/getCurrentAct.ts
@@ -1,11 +1,11 @@
 import Storage from "expo-storage";
 
 export const getCurrentAct = async (): Promise<string> => {
-    const currentAct = await Storage.getItem({key: 'currentAct'});
-    let defaultAct = 'startgame';
-    return currentAct || defaultAct;
-}
+  const currentAct = await Storage.getItem({ key: "currentAct" });
+  let defaultAct = "startgame";
+  return currentAct || defaultAct;
+};
 
 export const setCurrentAct = async (actKey: string) => {
-    await Storage.setItem({key: 'currentAct', value: actKey});
-}
+  await Storage.setItem({ key: "currentAct", value: actKey });
+};
