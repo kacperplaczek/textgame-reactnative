9df1e2c79ced49a11d41c43d477004f961d3a89b
xanaxdev <developer.xanax@gmail.com>
2025-04-21 14:29:47 +0200

    Remove & Create New: Plik kofniguracyjny aktów

--- lib/settings/acts.config.ts
+++ lib/settings/acts.config.ts
@@ -1,21 +1,24 @@
-// acts.config.ts
+import * as scenariuszAkt1 from "@/scenario/scenariuszAkt1";
+import * as scenariuszAkt2 from "@/scenario/scenariuszAkt2";
+import * as scenariuszAkt3 from "@/scenario/scenariuszAkt3";
+
 export const actsConfig = {
   startgame: {
     id: "startgame",
     title: "PROLOG",
     initialScene: "dzwoni_officer",
-    scenario: require("@/scenario/scenariuszAkt1"),
+    scenario: scenariuszAkt1, // ✅ to jest importowany moduł
   },
   "akt-1": {
     id: "akt-1",
     title: "AKT I",
     initialScene: "rozpoczecie_akt2",
-    scenario: require("@/scenario/scenariuszAkt2"),
+    scenario: scenariuszAkt2,
   },
   "akt-2": {
     id: "akt-2",
     title: "AKT II",
     initialScene: "rozpoczecie_aktu",
-    scenario: require("@/scenario/scenariuszAkt3"),
+    scenario: scenariuszAkt3,
   },
 };
