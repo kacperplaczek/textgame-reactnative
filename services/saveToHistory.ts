fcfbc9ba57ea31dbcc605e7b0928c520b1d796a4
xanaxdev <developer.xanax@gmail.com>
2025-04-27 13:11:55 +0200

    Move: Przeniesienie do servoices/ plików: saveToHistory.ts, scenerioLoader.ts, SchedulePushNotification.ts, soundController.ts

---
+++ services/saveToHistory.ts
@@ -0,0 +1,22 @@
+import Storage from "expo-storage";
+import { DialogueMessage } from "./useDialogue";
+
+export const saveToHistory = async (
+  actId: string,
+  message: DialogueMessage
+) => {
+  const key = "dialogue_history";
+  const existing = await Storage.getItem({ key });
+  const historyData = existing ? JSON.parse(existing) : {};
+
+  if (!historyData[actId]) {
+    historyData[actId] = [];
+  }
+
+  historyData[actId].push(message);
+
+  await Storage.setItem({
+    key,
+    value: JSON.stringify(historyData),
+  });
+};
