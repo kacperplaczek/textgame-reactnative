55187c3c51d3259bde450e7ca50656b63fe939b4
xanaxdev <developer.xanax@gmail.com>
2025-04-29 11:14:04 +0200

    Create: Utworzenie ustawień assetów dla dźwięku.

---
+++ settings/soundAssets.ts
@@ -0,0 +1,5 @@
+export const soundAssets = {
+  callphone: require("@/assets/sounds/phone-call.mp3"),
+  choice: require("@/assets/sounds/choice.wav"),
+  pustynia: require("@/assets/sounds/pustynia.mp3"),
+};
