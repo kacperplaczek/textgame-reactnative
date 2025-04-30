c8c00f0f6effead8e84ad85ff567fbe8aa8e7526
xanaxdev <developer.xanax@gmail.com>
2025-04-30 12:15:46 +0200

    Delete: Usunięcie niepotrzebnych ustawień dla EndActScreen

--- settings/screens/EndActScreens.ts
+++
@@ -1,5 +0,0 @@
-import ActEndScreen from '@/components/endactscreens/ActEndScreen';
-
-export const endActScreensMap: Record<string, React.FC<{ onContinue: () => void }>> = {
-    actEndScreen: ActEndScreen,
-};
