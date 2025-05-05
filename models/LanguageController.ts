cf5b54b1cdb1d62f1ba77d0d15d09c0b74869230
xanaxdev <developer.xanax@gmail.com>
2025-05-05 18:54:17 +0200

    Update: Zaktualizowanie LanguageController pod nowego Zustand Store

--- models/LanguageController.ts
+++ models/LanguageController.ts
@@ -1,21 +1,22 @@
 import * as Localization from "expo-localization";
 import Storage from "expo-storage";
 import type { Language } from "@/i18n/translations";
+import { useLanguageStore } from "@/store/LanguageStore";
 
 export const getCurrentLanguage = async (): Promise<Language> => {
   const storedLang = await Storage.getItem({ key: "lang" });
   console.log("📌 `Storage.getItem('lang')` zwróciło:", storedLang);
 
   if (storedLang === "pl" || storedLang === "en") {
+    useLanguageStore.getState().setLanguage(storedLang);
     return storedLang;
   }
 
-  // Nowy sposób pobierania języka systemowego
   const systemLang = Localization.getLocales()[0]?.languageCode;
-
   const defaultLang: Language = systemLang === "pl" ? "pl" : "en";
 
   await Storage.setItem({ key: "lang", value: defaultLang });
+  useLanguageStore.getState().setLanguage(defaultLang);
 
   return defaultLang;
 };
@@ -23,4 +24,5 @@ export const getCurrentLanguage = async (): Promise<Language> => {
 export const setLanguage = async (lang: Language) => {
   console.log("🌍 Ustawiam język systemowy:", lang);
   await Storage.setItem({ key: "lang", value: lang });
+  useLanguageStore.getState().setLanguage(lang);
 };
